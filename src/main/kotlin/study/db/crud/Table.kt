440347c3a1f74c0d9822f7b3d2ef09ee0ab35d8c
luke <gseonggyu968@gmail.com>
2026-01-21 21:13:32 +0900

    불필요한 폴더 삭제

--- src/main/kotlin/study/db/crud/Table.kt
+++
@@ -1,7 +0,0 @@
-package study.db.crud
-
-data class Table(
-    val tableName: String,
-    val dataType: Map<String, String>,
-    val value: Map<String, Any>,
-)
--- src/main/kotlin/study/db/storage_engine/Constants.kt
+++
@@ -1,5 +0,0 @@
-package study.db.storage_engine
-
-const val PAGE_SIZE = 16          // 한 페이지의 크기 (Byte)
-const val INT_SIZE = 4            // INT 타입의 크기 (Byte)
-const val DEFAULT_PAGE_COUNT = 6  // 기본 페이지 개수
